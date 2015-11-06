6aa73868e453a59be9274d697168b2117f32df84
Daniel <Jesus@Cybershade.org>
2015-11-06 23:16:42 +0000

    Added some Facades incase like, you wanna use laravel

---
+++ src/BattleShipsServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NoelDavies\BattleShips;
+
+use Illuminate\Support\ServiceProvider;
+
+class BattleShipsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['noeldavies-battleships-grid'] = $this->app->share(function($app)
+        {
+            return new Grid;
+        });
+        $this->app['noeldavies-battleships-ship'] = $this->app->share(function($app)
+        {
+            return new Ship;
+        });
+        $this->app['noeldavies-battleships-coordinate'] = $this->app->share(function($app)
+        {
+            return new Coordinate;
+        });
+
+
+        // Register Facade
+        $this->app->booting(function()
+        {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('BattleshipGrid', 'NoelDavies\BattleShips\Facades\Grid');
+            $loader->alias('BattleshipShip', 'NoelDavies\BattleShips\Facades\Ship');
+            $loader->alias('BattleshipPoint', 'NoelDavies\BattleShips\Facades\Coordinate');
+        });
+    }
+}
---
+++ src/Facades/Coordinate.php
@@ -0,0 +1,14 @@
+<?php namespace NoelDavies\Battleships\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Coordinate extends Facade {
+
+  /**
+   * Get the registered name of the component.
+   *
+   * @return string
+   */
+  protected static function getFacadeAccessor() { return 'noeldavies-battleships-coordinate'; }
+
+}
